ed80ec9f5e7046f761c0c43cbdac6b1fa26fa1ab
mbakerdeqode <mbaker@deqode.com>
2022-03-03 14:46:55 +0530

    feat: add user route and index file

---
+++ routes/user.route.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import userController from "../controller/user.controller";
+
+const router = express.Router();
+
+router.post("/userSearch", userController.userSearch);
+export { router as userRoute };
